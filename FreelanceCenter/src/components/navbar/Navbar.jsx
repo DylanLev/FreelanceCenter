1b039b54d3e14ae136544485dbaa747d923f8504
DylanLev <dylan.levy5@yahoo.com>
2024-03-12 12:53:37 +0200

    React Router Dom implementation

--- FreelanceCenter/src/components/navbar/Navbar.jsx
+++ FreelanceCenter/src/components/navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import "./Navbar.scss"
-//import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
     const [active, setActive] = useState(false);
     const [open, setOpen] = useState(false);
-  //const { pathname } = useLocation();
+  const { pathname } = useLocation();
   //const navigate = useNavigate()
 
   const isActive = () => {
@@ -25,13 +25,13 @@ const Navbar = () => {
     isSeller: true
   }
   return (
-    <div className={active ? "navbar active" : "navbar"}>
+    <div className={active || pathname!=='/' ? "navbar active" : "navbar"}>
         <div className="container">
             <div className="logo">
-                {/* <Link to='/'> */}
+                <Link to='/' className='link'>
                 <span className='text1'>Freelance</span>
                 <span className='text2'>Center</span>
-                {/* </Link> */}
+                </Link>
             </div>
             <div className="links">
                 <span>Business</span>
@@ -46,14 +46,14 @@ const Navbar = () => {
                         {open && <div className="options">
                             {currentUser?.isSeller && (
                                     <>
-                                    <span>Gigs</span>
-                                    <span>Add new Gig</span>
+                                    <Link className='link' to='/mygigs'>Gigs</Link>
+                                    <Link className='link' to='/add'>Add new Gig</Link>
                                     </>
                                 )
                             }
-                                <span>Orders</span>
-                                <span>Messages</span>
-                                <span>Logout</span>
+                                <Link className='link' to='/orders'>Orders</Link>
+                                <Link className='link' to='/messages'>Messages</Link>
+                                <Link className='link' to='/logout'>Logout</Link>
                             
                         </div>}
                     </div>
@@ -64,37 +64,37 @@ const Navbar = () => {
             </div>
         </div>
         <hr/>
-        {(active) && (
+        {(active || pathname!=='/') && (
         <>
           <hr />
           <div className="menu">
-            {/* <Link className="link menuLink" to="/"> */}
+            <Link className="link menuLink" to="/">
               Programming
-            {/* </Link> */}
-            {/* <Link className="link menuLink" to="/"> */}
+            </Link>
+            <Link className="link menuLink" to="/">
               Cyber
-            {/* </Link> */}
-            {/* <Link className="link menuLink" to="/"> */}
+            </Link>
+            <Link className="link menuLink" to="/">
               Hardware
-            {/* </Link> */}
-            {/* <Link className="link menuLink" to="/"> */}
+            </Link>
+            <Link className="link menuLink" to="/">
               AI Services
-            {/* </Link> */}
-            {/* <Link className="link menuLink" to="/"> */}
+            </Link>
+            <Link className="link menuLink" to="/">
               Digital Marketing
-            {/* </Link> */}
-            {/* <Link className="link menuLink" to="/"> */}
+            </Link>
+            <Link className="link menuLink" to="/">
               Embedded
-            {/* </Link> */}
-            {/* <Link className="link menuLink" to="/"> */}
+            </Link>
+            <Link className="link menuLink" to="/">
               Courses
-            {/* </Link> */}
-            {/* <Link className="link menuLink" to="/"> */}
+            </Link>
+            <Link className="link menuLink" to="/">
               Data Science
-            {/* </Link> */}
-            {/* <Link className="link menuLink" to="/"> */}
+            </Link>
+            <Link className="link menuLink" to="/">
               Web Design
-            {/* </Link> */}
+            </Link>
           </div>
           <hr />
         </>
