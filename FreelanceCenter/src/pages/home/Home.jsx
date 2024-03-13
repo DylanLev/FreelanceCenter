1b68cf91603f04964796dddabfeccf5930302f0a
DylanLev <dylan.levy5@yahoo.com>
2024-03-13 11:40:49 +0200

    Slide and Category Cards design

--- FreelanceCenter/src/pages/home/Home.jsx
+++ FreelanceCenter/src/pages/home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./Home.scss"
-
+import Slide from '../../components/slide/Slide'
+import CatCard from '../../components/catCard/CatCard'
+import {cards} from "../../data"
 const Home = () => {
   return (
-    <div className="home">Home</div>
+    <div className="home">
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map(card=>(
+          <CatCard key={card.id} item={card}/>
+        ))}
+      </Slide>
+    </div>
   )
 }
 
