db4b8b1e6129fca205fb4e73756f72bec22a194f
DylanLev <dylan.levy5@yahoo.com>
2024-06-06 11:42:11 +0300

    GigCard, Gig & Gigs components

--- FreelanceCenter/src/pages/gig/Gig.jsx
+++ FreelanceCenter/src/pages/gig/Gig.jsx
@@ -3,7 +3,27 @@ import "./Gig.scss"
 
 const Gig = () => {
   return (
-    <div className="gig">Gig</div>
+    <div className="gig">
+      <div className="container">
+        <div className="left">
+          <span className="breadCrumbs">FREELANCECENTER: GRAPHICS & DESIGN</span>
+          <h1>I will create ai generated art for you</h1>
+          <div className="user">
+            <img src="" alt="" />
+            <span>Avihai Levy</span>
+            <div className="stars">
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <span>5</span>
+            </div>
+          </div>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
   )
 }
 
