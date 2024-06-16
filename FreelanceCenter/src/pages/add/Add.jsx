3b6e3b7a8296cea5bb4214cf4a8007d6691a3549
DylanLev <dylan.levy5@yahoo.com>
2024-06-16 18:06:42 +0300

    "Add" gig page

--- FreelanceCenter/src/pages/add/Add.jsx
+++ FreelanceCenter/src/pages/add/Add.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
-import "./Add.scss"
+import React from "react";
+import "./Add.scss";
 
 const Add = () => {
   return (
-    <div className="add">Add</div>
-  )
-}
+    <div className="add">
+      <div className="container">
+        <h1>Add New Gig</h1>
+        <div className="sections">
+          <div className="info">
+            <label htmlFor="">Title</label>
+            <input
+              type="text"
+              placeholder="e.g. I will do something I'm really good at"
+            />
+            <label htmlFor="">Category</label>
+            <select name="cats" id="cats">
+              <option value="programming">Programming</option>
+              <option value="web">Web Design</option>
+              <option value="cyber">Cyber</option>
+              <option value="ai">Artificial Intelligence</option>
+              <option value="digital">Digital Marketing</option>
+              <option value="course">Course</option>
+              <option value="data">Data</option>
+              <option value="embedded">Embedded</option>
+              <option value="other">Other</option>
 
-export default Add
+
+            </select>
+            <label htmlFor="">Cover Image</label>
+            <input type="file" />
+            <label htmlFor="">Upload Images</label>
+            <input type="file" multiple />
+            <label htmlFor="">Description</label>
+            <textarea name="" id="" placeholder="Brief descriptions to introduce your service to customers" cols="0" rows="16"></textarea>
+            <button>Create</button>
+          </div>
+          <div className="details">
+            <label htmlFor="">Service Title</label>
+            <input type="text" placeholder="e.g. One-page web design" />
+            <label htmlFor="">Short Description</label>
+            <textarea name="" id="" placeholder="Short description of your service" cols="30" rows="10"></textarea>
+            <label htmlFor="">Delivery Time (e.g. 3 days)</label>
+            <input type="number" />
+            <label htmlFor="">Revision Number</label>
+            <input type="number" />
+            <label htmlFor="">Add Features</label>
+            <input type="text" placeholder="e.g. page design" />
+            <input type="text" placeholder="e.g. file uploading" />
+            <input type="text" placeholder="e.g. setting up a domain" />
+            <input type="text" placeholder="e.g. hosting" />
+            <label htmlFor="">Price</label>
+            <input type="number" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
