4824a2da157916e51a29492f5b3dc9eaa95be083
DylanLev <dylan.levy5@yahoo.com>
2024-06-21 17:45:28 +0300

    Routes, Controllers & modifications

--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js"
+import gigRoute from "./routes/gig.route.js"
+import orderRoute from "./routes/order.route.js"
+import conversationRoute from "./routes/conversation.route.js"
+import messageRoute from "./routes/message.route.js"
+import reviewRoute from "./routes/review.route.js"
+import authRoute from "./routes/auth.route.js"
 
 const app = express();
 dotenv.config();
@@ -14,6 +21,20 @@ try {
     console.log(error);
 }
 };
+
+//allow app to write from client side
+app.use(express.json());
+
+//Routes endpoints
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
+
+
 
 app.listen(8800, ()=>{
     connect();
